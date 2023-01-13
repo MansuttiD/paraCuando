553dd212e2286b3e6a05cbd09dd1468a1f93bbb7
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2023-01-13 12:54:16 -0600

    style: variables tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,48 @@ module.exports = {
       lg: '1200px',
       xl: '1536px',
     },
+    colors: {
+      primary: {
+        blue: '#1B4DB1',
+        yellow: '#F3F243',
+        pink: '#FF64BC',
+        black: '#000000',
+        blackLight: '#1A1E2E',
+        grayDark: '#6E6A6C',
+        gray: '#A7A6A7',
+        grayLight: '#D9D9D9',
+        grayLighter: '#F8F7FA',
+      },
+      'secondary-red': '#EF3F47',
+    },
+    fontFamily: {
+      sans: 'Roboto',
+    },
+    fontWeight: {
+      black: 900,
+      semibold: 600,
+      medium: 500,
+      normal: 400,
+    },
+    fontSize: {
+      xs: '14px',
+      sm: '15px',
+      base: '16px',
+      xl: '20px',
+      '2xl': '24px',
+      '5xl': '48px',
+    },
+    lineHeight: {
+      'leading-3': '16px',
+      'leading-4': '18px',
+      'leading-5': '19px',
+      'leading-6': '23px',
+      'leading-7': '28px',
+      'leading-8': '56px',
+    },
+    dropShadow: {
+      '1xl': '0px 2px 4px rgba(0,0,0,0.25)',
+    },
   },
   plugins: [],
 };
