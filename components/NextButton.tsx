1a63c0e5db7ee4bbaeb1670c063f926b8729c70f
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2023-01-14 23:02:27 -0600

    ci: atoms

---
+++ components/NextButton.tsx
@@ -0,0 +1,8 @@
+export default function NextButton() {
+    return (
+      <button className="w-[124px]  h-12 p-[18px] h500-normal-16px text-white flex justify-center items-center bg-primary-blue rounded-[31px] font-semibold ">
+        Siguiente
+      </button>
+    );
+  }
+  
