a5ff764411b4aa6cd3c6e74efed63a12b0ab3246
MansuttiD <daniel.mansutti@gmail.com>
2023-01-16 16:14:39 -0400

    ci: header and footer create

--- components/NextButton.tsx
+++ components/NextButton.tsx
@@ -1,8 +1,7 @@
-export default function NextButton({action}) {
-    return (
-      <button className="w-[124px]  h-12 p-[18px] h500-normal-16px text-white flex justify-center items-center bg-primary-blue rounded-[31px] font-semibold ">
-        {action}
-      </button>
-    );
-  }
-  
+export default function NextButton() {
+  return (
+    <button className="w-[124px]  h-12 p-[18px] h500-normal-16px text-white flex justify-center items-center bg-primary-blue rounded-[31px] font-semibold ">
+      Siguiente
+    </button>
+  );
+}
