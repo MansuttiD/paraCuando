e8e0f72c35aa63a59f546ada446b9bec7b0bcdcb
MansuttiD <daniel.mansutti@gmail.com>
2023-01-16 16:11:55 -0400

    ci: header and footer create

--- components/Label.tsx
+++ components/Label.tsx
@@ -1,8 +1,7 @@
-export default function Label({category}) {
-    return (
-      <span className="rounded-3xl h500-medium-14px inline-block py-4 px-5 bg-white text-primary-gray border-[1px] border-primary-gray w-fit ">
-        {category}
-      </span>
-    );
-  }
-  
+export default function Label() {
+  return (
+    <span className="rounded-3xl h500-medium-14px inline-block py-4 px-5 bg-white text-primary-gray border-[1px] border-primary-gray w-fit ">
+      Marcas y Tiendas
+    </span>
+  );
+}
