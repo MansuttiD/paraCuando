a958791d99d18dce43967ff60e927d4b97e58bd6
MansuttiD <daniel.mansutti@gmail.com>
2023-01-16 16:02:01 -0400

    ci: header and footer create

--- components/Layoud.tsx
+++ components/Layoud.tsx
@@ -1,13 +1,12 @@
-import Header from "./Header"
-import Navbar from "./Navbar"
+import Header from './Header';
+import Navbar from './Navbar';
 
-export default function Layoud ({ children })  {
-
-    return (
-        <>
-            <Header/>
-                { children }
-            <Navbar/>
-        </>
-    )
-  }
+export default function Layoud({ children }) {
+  return (
+    <>
+      <Header email={'daniel.mansutti@gmail.com'} />
+      {children}
+      <Navbar />
+    </>
+  );
+}
