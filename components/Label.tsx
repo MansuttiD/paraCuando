56cedee7913f417b4182560367573de1e9726f8e
Daniel1264 <94574017+Daniel1264@users.noreply.github.com>
2023-01-17 20:03:35 -0600

    ci: add component eventcard

--- components/Label.tsx
+++ components/Label.tsx
@@ -1,4 +1,8 @@
-export default function Label({ category }) {
+type AppProps = {
+  category: string;
+};
+
+export default function Label({ category }: AppProps) {
   return (
     <span className="rounded-3xl h500-medium-14px inline-block py-4 px-5 bg-white text-primary-gray border-[1px] border-primary-gray w-fit ">
       {category}
