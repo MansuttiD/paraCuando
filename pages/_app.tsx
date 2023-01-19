d4e88119bb6d3895bc04f4319aa92f21eb4feb29
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2023-01-19 02:06:49 -0600

    ci: login and singup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layoud>
-      <Component  {...pageProps}/>
+      <Component {...pageProps} />
     </Layoud>
-  )
+  );
 }
